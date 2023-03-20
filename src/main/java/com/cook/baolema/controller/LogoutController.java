d86bd24f42dbd0dd0afd425cacdfa72af5212475
WSKNOO <wangkaisn@outlook.com>
2023-03-20 09:52:00 +0800

    add

---
+++ src/main/java/com/cook/baolema/controller/LogoutController.java
@@ -0,0 +1,10 @@
+package com.cook.baolema.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/logout")
+public class LogoutController {
+
+}
--- src/main/java/com/cook/baolema/service/OrderInfoService.java
+++ src/main/java/com/cook/baolema/service/OrderInfoService.java
@@ -20,4 +20,6 @@ public interface OrderInfoService {
 
     Integer checkOrderID(Integer customerID);
 
+    List<OrderInfo> selectHistoryOrder(Integer customerID);
+
 }

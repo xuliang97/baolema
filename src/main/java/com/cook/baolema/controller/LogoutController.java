a8749f90a2e691289721ad4871ee4df02b45c8a0
WSKNOO <wangkaisn@outlook.com>
2023-03-20 10:02:05 +0800

    add

--- src/main/java/com/cook/baolema/controller/LogoutController.java
+++ src/main/java/com/cook/baolema/controller/LogoutController.java
@@ -1,10 +1,30 @@
 package com.cook.baolema.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpSession;
 
 @RestController
 @RequestMapping("/logout")
 public class LogoutController {
+    @PostMapping("/manager")
+    public String managerLogout(HttpSession session){
+        session.removeAttribute("managerInfo");
+        return "redirect:/login/manager";
+    }
+
+    @PostMapping("/customer")
+    public String customerLogout(HttpSession session){
+        session.removeAttribute("customerInfo");
+        return "redirect:/login/customer";
+    }
+
+    @PostMapping("/chef")
+    public String chefLogout(HttpSession session){
+        session.removeAttribute("chefInfo");
+        return "redirect:/login/chef";
+    }
 
 }

2cc3e39a5f56ac2909424b7f7f30727c1c830da5
xuliang <1721436737@qq.com>
2023-03-20 16:10:24 +0800

    uuid属性，token实现

--- src/main/java/com/cook/baolema/controller/LoginController.java
+++ src/main/java/com/cook/baolema/controller/LoginController.java
@@ -4,11 +4,14 @@ import com.cook.baolema.pojo.*;
 import com.cook.baolema.service.ChefService;
 import com.cook.baolema.service.CustomerService;
 import com.cook.baolema.service.ManagerService;
+import com.cook.baolema.utils.JwtUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author 徐亮
@@ -29,47 +32,84 @@ public class LoginController {
         String phoneNumber = request.getParameter("phoneNumber");
         String password = request.getParameter("password");
         Manager manager = managerService.selectByPhoneAndPwd(phoneNumber, password);
-        Integer code = manager != null ? Code.LOGIN_OK : Code.LOGIN_ERR;
-        String msg = manager != null ? "登录成功！" : "手机号或密码错误！";
-        if(manager != null){
-            //如果登录成功，将用户的信息（用户对象）保存到session中
-            HttpSession session = request.getSession();
-            session.setAttribute("managerInfo",manager);
+        if(manager == null){
+            //如果管理员不存在，那么就直接返回
+            return new Result(Code.LOGIN_ERR,null,"手机号或密码错误");
         }
-        return new Result(code, null, msg);
 
+//        if(manager != null){
+//            //如果登录成功，将用户的信息（用户对象）保存到session中
+//            HttpSession session = request.getSession();
+//            session.setAttribute("managerInfo",manager);
+//        }
+        //如果查询到对应数据，执行生成token的代码
+        Map<String,Object> map = new HashMap<>();
+        try{
+            Map<String,String> payload =  new HashMap<>();
+            payload.put("phoneNumber",phoneNumber);
+            payload.put("password",password);
+            // 生成JWT的令牌
+            String token = JwtUtils.createToken(payload);
+            // 响应token
+            map.put("token",token);
+        }catch (Exception e){
+            map.put("msg",e.getMessage());
+        }
+        return new Result(Code.LOGIN_OK,map,"登录成功");
     }
+
     @PostMapping("/customer")
     public Result customerLogin(HttpServletRequest request){
         String phoneNumber = request.getParameter("phoneNumber");
-//        System.out.println(phoneNumber);
-
         String password = request.getParameter("password");
-//        System.out.println(password);
         Customer customer = customerService.selectByPhoneAndPwd(phoneNumber, password);
-
-        Integer code = customer != null ? Code.LOGIN_OK : Code.LOGIN_ERR;
-        String msg = customer != null ? "登录成功！" : "手机号或密码错误！";
-        if(customer != null){
-            HttpSession session = request.getSession();
-            session.setAttribute("customerInfo",customer);
+        if(customer == null){
+            return new Result(Code.LOGIN_ERR,null,"用户名或密码错误");
         }
-        return new Result(code, null, msg);
 
+//        if(customer != null){
+//            HttpSession session = request.getSession();
+//            session.setAttribute("customerInfo",customer);
+//        }
+        Map<String,Object> map = new HashMap<>();
+        try{
+            Map<String,String> payload =  new HashMap<>();
+            payload.put("phoneNumber",phoneNumber);
+            payload.put("password",password);
+            // 生成JWT的令牌
+            String token = JwtUtils.createToken(payload);
+            // 响应token
+            map.put("token",token);
+        }catch (Exception e){
+            map.put("msg",e.getMessage());
+        }
+        return new Result(Code.LOGIN_OK,map,"登录成功");
     }
+
     @PostMapping("/chef")
-    public Result chefLogin(HttpServletRequest request){
+    public Result chefLogin(HttpServletRequest request) {
         String phoneNumber = request.getParameter("phoneNumber");
         String password = request.getParameter("password");
         Chef chef = chefService.selectByPhoneAndPwd(phoneNumber, password);
-        Integer code = chef != null ? Code.LOGIN_OK : Code.LOGIN_ERR;
-        String msg = chef != null ? "登录成功！" : "手机号或密码错误！";
-        if(chef != null){
-            HttpSession session = request.getSession();
-            session.setAttribute("chefInfo",chef);
+        if (chef == null) {
+            return new Result(Code.LOGIN_ERR, null, "用户名或密码错误");
         }
-        return new Result(code, null, msg);
+//        if(chef != null){
+//            HttpSession session = request.getSession();
+//            session.setAttribute("chefInfo",chef);
+//        }
+        Map<String,Object> map = new HashMap<>();
+        try{
+            Map<String,String> payload =  new HashMap<>();
+            payload.put("phoneNumber",phoneNumber);
+            payload.put("password",password);
+            // 生成JWT的令牌
+            String token = JwtUtils.createToken(payload);
+            // 响应token
+            map.put("token",token);
+        }catch (Exception e){
+            map.put("msg",e.getMessage());
+        }
+        return new Result(Code.LOGIN_OK,map,"登录成功");
     }
-
-
 }

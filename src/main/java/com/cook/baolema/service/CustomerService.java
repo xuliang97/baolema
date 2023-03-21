b21ab92d390b6e0d55072bd81c5edeb449882d2e
QuenKar <47681251+QuenKar@users.noreply.github.com>
2023-03-21 14:38:54 +0800

    feat CustomerDao @upadte operations.

--- src/main/java/com/cook/baolema/service/CustomerService.java
+++ src/main/java/com/cook/baolema/service/CustomerService.java
@@ -4,19 +4,28 @@ import com.cook.baolema.pojo.Customer;
 import com.github.pagehelper.*;
 
 
-
 import java.util.List;
 
 public interface CustomerService {
     List<Customer> selectAll();
+
     Customer selectByID(Integer id);
+
     boolean update(Customer customer);
+
     boolean save(Customer customer);
+
     boolean deleteByID(Integer id);
 
     Customer selectByPhoneAndPwd(String phoneNumber, String password);
-    Customer selectByPhoneNumber(String phoneNumber);
-    PageInfo<Customer> selectAllByPage(int pageNum, int pageSize);
-    boolean updatePwd(String password,Integer customerID);
 
+    Customer selectByPhoneNumber(String phoneNumber);
+
+    PageInfo<Customer> selectAllByPage(int pageNum, int pageSize);
+
+    boolean updatePwd(String password, Integer customerID);
+
+    boolean updateRankByCustomerID(Integer customerID, Integer rank);
+
+    boolean updateAccumulatedAmountByCustomerID(Integer customerID, Float amount);
 }
---
+++ src/test/java/com/cook/baolema/service/CustomerServiceTest.java
@@ -0,0 +1,25 @@
+//package com.cook.baolema.service;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//import static org.junit.jupiter.api.Assertions.*;
+//
+//@SpringBootTest
+//class CustomerServiceTest {
+//
+//    @Autowired
+//    private CustomerService customerService;
+//
+//
+//    @Test
+//    void updateRankByCustomerID() {
+//        customerService.updateRankByCustomerID(1, 1);
+//    }
+//
+//    @Test
+//    void updateAccumulatedAmountByCustomerID() {
+//        customerService.updateAccumulatedAmountByCustomerID(1,12.8f);
+//    }
+//}
